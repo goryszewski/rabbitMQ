bab3be818d40d9755d0f1c483d55b0a3d925c74d
Michal Goryszewski <goryszewski@gmail.com>
2024-12-25 22:48:12 +0100

    test flask and rabbitmq

--- send.py
+++ send.py
@@ -1,9 +1,10 @@
 from lib.rabbit import Queue
+from flask import Flask
 from time import sleep
-
+from dotenv import load_dotenv
 import signal
-import sys
-
+import sys,os
+load_dotenv()
 import argparse
 
 # import atexit sprawdzic DOTO
@@ -16,27 +17,32 @@ def signal_handler(signal, frame):
     global STATE
     STATE = True
 
+app = Flask(__name__)
 
-signal.signal(signal.SIGINT, signal_handler)
+# signal.signal(signal.SIGINT, signal_handler)
 
-parser = argparse.ArgumentParser()
-parser.add_argument("host", help="host rabbitmq")
-args = parser.parse_args()
+# parser = argparse.ArgumentParser()
+# parser.add_argument("host", help="host rabbitmq")
+# args = parser.parse_args()
+objectQ = Queue(host=os.environ.get("RABBITMQ_HOST"), queue=os.environ.get("RABBITMQ_QUEUE_NAME"))
+
+@app.route("/<name>")
+def hello(name):
+    objectQ.send(f"test1  -{name}")
+    return "done"
+
+# def main():
+#     c = 1
+#     while True:
+#         if STATE:
+#             break
+#         c = c + 1
+        
+#         sleep(0.1)
+#         print(f"Send msg")
 
 
-def main():
-    objectQ = Queue(host=args.host, queue="hello")
-    c = 1
-    while True:
-        if STATE:
-            break
-        c = c + 1
-        objectQ.send(f"test1{c}")
-        sleep(0.1)
-        print(f"Send msg")
+# if __name__ == "__main__":
+#     main()
 
-
-if __name__ == "__main__":
-    main()
-
-    print("normal end")
+#     print("normal end")
