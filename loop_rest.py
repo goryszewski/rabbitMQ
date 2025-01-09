6c7237667c3f6e4615b8accb17857b3fcf78f5b0
Michal Goryszewski <goryszewski@gmail.com>
2025-01-09 22:51:41 +0100

    add output for looper

--- loop_rest.py
+++ loop_rest.py
@@ -4,7 +4,8 @@ url: str = "http://127.0.0.1:5000/name"
 headers = {"Content-Type": "application/json"}
 data = {"data1": 1}
 while True:
-
-    test = requests.post(url=url, headers=headers, json=data)
-    print("ping")
+    response = requests.post(url=url, headers=headers, json=data)
+    print(f"Code: {response.status_code}")
+    # print(f"Headers: {response.headers}")
+    print(f"Headers: {response.json()}")
     time.sleep(1)
